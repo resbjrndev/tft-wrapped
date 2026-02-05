f2b741ccc12e8ad3f50ef243aa528d431faeab05
Robin Esbjörnsson <Robin.Esbjornsson@hotmail.com>
2026-02-05 16:02:53 +0100

    add api route

--- src/app/api/wrapped/route.ts
+++ src/app/api/wrapped/route.ts
@@ -1,15 +1,23 @@
+import { RiotAPIService } from "@/app/lib/riot-api";
 import { NextRequest, NextResponse } from "next/server";
-import type { NextApiRequest, NextApiResponse } from 'next'
 
-type responseData = {
-    gameName: string; 
-    tagLine: string;
-    year: number;
-}
-
-export default function handler( req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'POST') {
-
-        
+export async function POST(request: NextRequest) {
+    const riotAPI = new RiotAPIService();
+    try {
+        const { gameName, tagLine, year } = await request.json();
+        if (!gameName || !tagLine || !year) {
+            return NextResponse.json(
+                { error: 'Missing required fields' },
+                { status: 400 }
+            );
+        }
+        let account = await riotAPI.getAccountByRiotId(gameName, tagLine)
+        let matches = await riotAPI.getAllMatchesForYear(account.puuid, year);
+        return NextResponse.json({ account, matches });
+    } catch (error) {
+        return NextResponse.json(
+            { error: 'Something went wrong' },
+            { status: 500 }
+        );
     }
 }
